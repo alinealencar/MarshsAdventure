2cd173fed8cb33c8e5cf343d3f50e7b1dee40b3f
Aline <aline.nevesalencar@georgebrown.ca>
2017-12-16 21:37:01 -0500

    Add Start scene

---
+++ Assets/Scripts/Level1/PauseController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class PauseController : MonoBehaviour, IPointerClickHandler {
+	[SerializeField]
+	public Image continueImage;
+	[SerializeField]
+	public Image pauseImage;
+
+	public void OnPointerClick(PointerEventData eventData)
+	{
+		//When click on pause, set the timeScale of the game to 0 
+		//(all animations stops and the player cannot move anymore)
+		Time.timeScale = 0;
+
+		//Hide pause button
+		pauseImage.gameObject.SetActive(false);
+
+		//Show images
+		continueImage.gameObject.SetActive(true);
+
+	}
+}
