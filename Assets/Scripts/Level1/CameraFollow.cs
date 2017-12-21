ecd7a55735ae1344c5eb3f7017e4f2c7c0ffb59f
kogiya <kie.ogiya@gmail.com>
2017-12-21 10:57:04 -0500

    Add camera follow stop and marsh's boundaries

--- Assets/Scripts/Level1/CameraFollow.cs
+++ Assets/Scripts/Level1/CameraFollow.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour {
 	[SerializeField]
 	Transform target = null;
+	[SerializeField]
+	float startFollow = -6.5f;
+	[SerializeField]
+	float endFollow = 101.76f;
 
 	// Use this for initialization
 	void Start () {
@@ -12,11 +16,14 @@ public class CameraFollow : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
+		//Make the camera stop at the beginning and at the end of the level
+		if(target.position.x > startFollow && target.position.x < endFollow) {
 		gameObject.transform.position = 
 			new Vector3 (//camera follows only for x direction
 				target.position.x, 
 				gameObject.transform.position.y,
 				gameObject.transform.position.z
 			);
+		}
 	}
 }
