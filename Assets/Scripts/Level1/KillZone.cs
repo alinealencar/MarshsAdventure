0def7946a4c93cfb5a664fc9ff4bb9583bcb4933
alyssavillacete <alyssavillacete@yahoo.com>
2018-01-04 10:57:02 -0500

    Fix CameraFollow in level 3

--- Assets/Scripts/Level1/KillZone.cs
+++ Assets/Scripts/Level1/KillZone.cs
@@ -7,11 +7,14 @@ public class KillZone : MonoBehaviour {
 	[SerializeField]
 	Transform spawnPoint = null;
 
+
 	public void OnCollisionEnter2D(Collision2D other){
 		other.transform.position = spawnPoint.position;
 		Rigidbody2D rb = other.gameObject.GetComponent<Rigidbody2D> ();
 		if (rb) {
 			rb.velocity = Vector2.zero;
 		}
+			
 	}
-}
+	}
+
